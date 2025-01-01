6f974b03d5526f6c9741df827d0cc83685721ab6
Ludonovan <lucasdonovan@live.com>
2025-01-01 04:06:02 -0500

    Initial commit

---
+++ Adder2/fadd.v
@@ -0,0 +1,16 @@
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);//
+
+    add16 first (  );
+    add16 secnd (  );
+    
+endmodule
+
+module add1 ( input a, input b, input cin,   output sum, output cout );
+
+// Full adder module here
+
+endmodule
